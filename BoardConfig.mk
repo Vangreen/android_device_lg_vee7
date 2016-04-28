f959afc6d6257a6d42a0e7ca25409edb00939023
Vangreen <pwangryn@op.pl>
2016-04-28 16:48:48 +0200

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,146 @@
+# inherit from the proprietary version
+-include vendor/lge/vee7/BoardConfigVendor.mk
+
+# Compiler flags
+TARGET_GLOBAL_CFLAGS += -mfloat-abi=softfp -mfpu=neon-vfpv4 -mtune=cortex-a5
+TARGET_GLOBAL_CPPFLAGS += -mfloat-abi=softfp -mfpu=neon-vfpv4 -mtune=cortex-a5
+COMMON_GLOBAL_CFLAGS += -DUSE_MDP3
+COMMON_GLOBAL_CFLAGS += -DLPA_DEFAULT_BUFFER_SIZE=480
+
+# Targets
+TARGET_AVOID_DRAW_TEXTURE_EXTENSION := true
+TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := vee7
+TARGET_BOARD_PLATFORM := msm7x27a
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_GRALLOC_USES_ASHMEM := true
+
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a5
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CORTEX_CACHE_LINE_32 := true
+ARCH_ARM_HAVE_32_BYTE_CACHE_LINES := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/lge/vee7
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01300000
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_PAGESIZE := 4096
+TARGET_KERNEL_CONFIG := cyanogenmod_vee7_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.hardware=vee7 androidboot.selinux=permissive
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 12582912
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1270874112
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1941962752
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_VOLD_MAX_PARTITIONS := 27
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Qualcomm hardware
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+TARGET_USES_QCOM_BSP := true
+TARGET_USES_ION := true
+
+# GPS
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+
+# Bionic
+BOARD_USES_LEGACY_MMAP := true
+
+# Media
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+COMMON_GLOBAL_CFLAGS += -DBOARD_CANT_REALLOCATE_OMX_BUFFERS
+
+# Audio
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+
+# Display
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+USE_OPENGL_RENDERER := true
+TARGET_DISPLAY_USE_RETIRE_FENCE := true
+HWUI_COMPILE_FOR_PERF := true
+
+# Hardware
+BOARD_HARDWARE_CLASS := device/lge/vee7/cmhw
+
+# BT
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/vee7/bluetooth
+
+# Light
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Use dlmalloc instead of jemalloc for mallocs on low-ram targets
+MALLOC_IMPL := dlmalloc
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+	WITH_DEXPREOPT := true
+endif
+WITH_DEXPREOPT_PIC := true
+DONT_DEXPREOPT_PREBUILTS := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/lge/vee7/sepolicy
+
+# Wlan
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_EXT_MODULE_PATH := "/system/lib/modules/librasdioif.ko"
+WIFI_EXT_MODULE_NAME := "librasdioif"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WIFI_DRIVER_FW_PATH_PARAM := "/data/misc/wifi/fwpath"
+
+# FSTAB
+TARGET_RECOVERY_FSTAB := device/lge/vee7/rootdir/fstab.vee7
+
+# Charger
+COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"androidboot.mode"' -DBOARD_CHARGING_CMDLINE_VALUE='"charger"'
+
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+TARGET_BOOTANIMATION_HALF_RES := true
+
+# For assert
+TARGET_OTA_ASSERT_DEVICE := p710,vee7,P710
+
+# RIL
+BOARD_RIL_CLASS += ../../../device/lge/vee7/ril
+TARGET_RIL_VARIANT := legacy
+COMMON_GLOBAL_CFLAGS += -DRIL_SUPPORTS_SEEK
+COMMON_GLOBAL_CFLAGS += -DRIL_VARIANT_LEGACY
+
+# Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_vee7
+TARGET_LIBINIT_DEFINES_FILE := device/lge/vee7/init/init_vee7.cpp
+
+COMPILE_WITH_JACK := false
